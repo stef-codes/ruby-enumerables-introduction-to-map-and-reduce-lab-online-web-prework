d7f77b97bf936f93aabb957b92edb78db09abfd2
Stefanie Davis <davisstefanie2010@gmail.com>
2019-09-19 21:58:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -22,3 +22,8 @@ end
 def reduce_to_total(source_array,starting_point)
   source_array.reduce(starting_point) { |sum, n| sum + n }
 end 
+
+# all true
+def reduce_to_all_true(source_array)
+  
+end 
